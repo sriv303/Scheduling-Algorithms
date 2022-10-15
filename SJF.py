d4b74d92a96cc6ef7d07bd180987847a4b6f2188
sriv3021 <abi.vemulapalli@gmail.com>
2022-10-15 16:21:51 +0100

    Added sorting based on burst time

--- SJF.py
+++ SJF.py
@@ -1,10 +1,13 @@
 from JobClass import Job, joblist
 
 p1 = Job(3,0) #creating job
-p2 = Job(2,1)
-p3 = Job(7,2)
-p4 = Job(8,3)
-p5 = Job(9,6)
-p6 = Job(2, 5)
+p2 = Job(2,0)
+p3 = Job(7,0)
+p4 = Job(8,0)
+p5 = Job(9,0)
+p6 = Job(2,0)
 
 jobl = joblist
+
+jobl.sort(key = lambda x:x.burstTime)
+print(*[x.burstTime for x in jobl])
